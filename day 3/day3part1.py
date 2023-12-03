6a2842811484aebef6585f678edc3316c01b44ab
Hien1725 <131920075+Hien1725@users.noreply.github.com>
2023-12-03 17:16:16 +0100

    Version fonctionnelle
    Correction du bug ne prenant pas en compte qu'un même nombre peut se répéter sur une même ligne

--- day 3/day3part1.py
+++ day 3/day3part1.py
@@ -1,23 +1,22 @@
 def get_position_symbols(s,y):
+    """Extracts positions of symbols from a line."""
     positions = []
     list_of_char = list(enumerate(s))
     for (index, char) in list_of_char:
-        #if char != '.' and not char.isdigit():
-        if char in '*/=+%@#&-$':
+        if char != '.' and not char.isdigit():
+        #if char in '*/=+%@#&-$':
             
             positions.append((index, y))
 
     return positions
 
 def get_positions_around_a_number(x, y, length):
+    """Calculates positions around a number in a matrix."""
     positions = []
     y_left_upper_corner = y - 1
     y_right_low_corner = y + 1
     x_left_upper_corner = x - 1
     x_right_low_corner = x + length
-
-    left_upper_corner = (x_left_upper_corner, y_left_upper_corner)
-    right_low_corner = (x_right_low_corner, y_right_low_corner)
     
     #positions à gauche et à droite
     positions.append((x - 1, y))
@@ -31,20 +30,43 @@ def get_positions_around_a_number(x, y, length):
     return positions
 
 def is_a_tuple_from_list1_in_list2_of_tuple(list1, list2):
+    """Checks if any tuple from list1 is in list2."""
     for tup in list1:
         if tup in list2:
             return True
     return False
 
+def get_indexes_and_numbers(line):
+    """Finds indexes and corresponding numbers in a line."""
+    last_index = len(line) - 1
 
-path = "C:\\Users\\duche\\Desktop\\Advent of Code 2023\\day 3\\input_test.txt"
+    number_positions = []
+
+    whole_number = ''
+    is_first = True
+    for index, char in enumerate(line):
+        if char.isdigit():
+            if is_first == True:
+                i = index
+            whole_number += char
+            is_first = False
+
+            if index == last_index or not line[index + 1].isdigit():
+                number_positions.append((i, int(whole_number)))
+
+        else:
+            is_first = True
+            whole_number = ''
+
+    return number_positions
+
 path = "C:\\Users\\duche\\Desktop\\Advent of Code 2023\\day 3\\input.txt"
 
 positions_all_symbols = []
 with open(path) as file:
     data = file.readlines()
     for (index, line) in enumerate(data):
-        line = line.replace('\n','')
+        line = line.replace('\n', '')
         positions_all_symbols += get_position_symbols(line, index)
 
 count = 0
@@ -54,26 +76,18 @@ with open(path) as file:
         line = line.replace('*','.')
         line = line.replace('\n','')
         
-        # Pas moyen de comprendre pourquoi les + et - font bugger le système. Je les enlève
-        line = line.replace('+','.')
-        line = line.replace('-','.')
-
-        # D'autres font aussi bugger le système. J'enlève tout.
         for char in line:
-            if char in '*/=+%@#&-$':
+            if char in '+-*/=+%@#&-$':
                 line = line.replace(char,'.')
+        
+        # attention, si plusieurs fois la même valeur dans une ligne, il ne faut pas compter encore et encore la première
+        list_tuple_numbers_in_line = get_indexes_and_numbers(line)
 
-        list_number_in_line = [int(s) for s in line.split('.') if s.isdigit()]
-
-        for num in list_number_in_line:
+        for i_num, num in list_tuple_numbers_in_line:
             string_number = str(num)
-            i = line.find(str(string_number))
             length = len(string_number)
 
-            if is_a_tuple_from_list1_in_list2_of_tuple(get_positions_around_a_number(i, index, length), positions_all_symbols) == True:
+            if is_a_tuple_from_list1_in_list2_of_tuple(get_positions_around_a_number(i_num, index, length), positions_all_symbols):
                 count += num
-
-            #day 2
-        
 
 print(count)
